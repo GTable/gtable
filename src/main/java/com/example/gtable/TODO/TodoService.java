97ffcf9d76b8974f7a8c574677e80a687e220daf
jeonghyemin <hyem5019@gmail.com>
2025-05-30 16:09:08 +0900

    Feat : CodeRabbit 한글 적용 테스트(3)
    - 테스트.....(3)

--- src/main/java/com/example/gtable/TODO/TodoService.java
+++ src/main/java/com/example/gtable/TODO/TodoService.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
     public String add() {
         String todo = "TODO";
         String todoList = "TODO LIST";
