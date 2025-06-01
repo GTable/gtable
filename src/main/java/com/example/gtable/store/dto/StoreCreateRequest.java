7e90e6ea16729ffe9676644dfa5314a4c5a7045b
Jihun Kim <zh1229@naver.com>
2025-06-01 21:42:28 +0900

    feat: inherit BaseTimeEntity in StoreCreateRequest dto

--- src/main/java/com/example/gtable/store/dto/StoreCreateRequest.java
+++ src/main/java/com/example/gtable/store/dto/StoreCreateRequest.java
@@ -1,36 +1,39 @@
 package com.example.gtable.store.dto;
 
+import com.example.gtable.global.entity.BaseTimeEntity;
 import com.example.gtable.store.model.Store;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class StoreCreateRequest {
+public class StoreCreateRequest extends BaseTimeEntity {
 
-    @NotNull
-    private Long departmentId;
+	@NotNull
+	private Long departmentId;
 
-    @NotBlank
-    private String name;
+	@NotBlank
+	private String name;
 
-    private String location;
+	private String location;
 
-    private String description;
+	private String description;
 
-    private String storeImageUrl;
+	private String storeImageUrl;
 
-
-    public Store toEntity() {
-        return Store.builder()
-                .departmentId(departmentId)
-                .name(name)
-                .location(location)
-                .description(description)
-                .storeImageUrl(storeImageUrl)
-                .build();
-    }
+	public Store toEntity() {
+		return Store.builder()
+			.departmentId(departmentId)
+			.name(name)
+			.location(location)
+			.description(description)
+			.storeImageUrl(storeImageUrl)
+			.isActive(false)
+			.build();
+	}
 }
