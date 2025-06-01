af600f2801a55663ac9e2651ffaf70bcb0f764e7
Jihun Kim <zh1229@naver.com>
2025-06-01 21:42:35 +0900

    feat: inherit BaseTimeEntity in StoreCreateResponse dto

--- src/main/java/com/example/gtable/store/dto/StoreCreateResponse.java
+++ src/main/java/com/example/gtable/store/dto/StoreCreateResponse.java
@@ -1,35 +1,38 @@
 package com.example.gtable.store.dto;
 
-import com.example.gtable.store.model.Store;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-
 import java.time.LocalDateTime;
 
+import com.example.gtable.global.entity.BaseTimeEntity;
+import com.example.gtable.store.model.Store;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 @AllArgsConstructor
 @Builder
-public class StoreCreateResponse {
+public class StoreCreateResponse extends BaseTimeEntity {
 
-    private Long storeId;
-    private Long departmentId;
-    private String name;
-    private String location;
-    private String description;
-    private String storeImageUrl;
-    private Boolean isActive;
-    private LocalDateTime createdAt;
+	private Long storeId;
+	private Long departmentId;
+	private String name;
+	private String location;
+	private String description;
+	private String storeImageUrl;
+	private Boolean isActive;
+	private LocalDateTime createdAt;
 
-
-    public static StoreCreateResponse fromEntity(Store store) {
-        return StoreCreateResponse.builder()
-                .storeId(store.getStoreId())
-                .departmentId(store.getDepartmentId())
-                .name(store.getName())
-                .location(store.getLocation())
-                .description(store.getDescription())
-                .storeImageUrl(store.getStoreImageUrl())
-                .isActive(store.getIsActive())
-                .createdAt(store.getCreatedAt())
-                .build();
-    }
+	public static StoreCreateResponse fromEntity(Store store) {
+		return StoreCreateResponse.builder()
+			.createdAt(store.getCreatedAt())
+			.storeId(store.getStoreId())
+			.departmentId(store.getDepartmentId())
+			.name(store.getName())
+			.location(store.getLocation())
+			.description(store.getDescription())
+			.storeImageUrl(store.getStoreImageUrl())
+			.isActive(store.getIsActive())
+			.build();
+	}
 }
