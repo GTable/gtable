dbe9fefdd670378ed6dc89053778bdeb2d281acf
Jihun Kim <zh1229@naver.com>
2025-06-02 17:58:50 +0900

    feat(store): add methods to query non-deleted stores in repository

--- src/main/java/com/example/gtable/store/repository/StoreRepository.java
+++ src/main/java/com/example/gtable/store/repository/StoreRepository.java
@@ -1,9 +1,17 @@
 package com.example.gtable.store.repository;
 
-import com.example.gtable.store.model.Store;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.gtable.store.model.Store;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+	List<Store> findAllByDeletedFalse();
+
+	Optional<Store> findByStoreIdAndDeletedFalse(Long storeId);
 }
