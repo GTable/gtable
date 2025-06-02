b00d5bc1e405e32ee2e218bde18892742f533f06
Jihun Kim <zh1229@naver.com>
2025-06-02 17:59:58 +0900

    feat(store): add get, update, delete methods to StoreService interface

--- src/main/java/com/example/gtable/store/service/StoreService.java
+++ src/main/java/com/example/gtable/store/service/StoreService.java
@@ -2,8 +2,20 @@ package com.example.gtable.store.service;
 
 import com.example.gtable.store.dto.StoreCreateRequest;
 import com.example.gtable.store.dto.StoreCreateResponse;
+import com.example.gtable.store.dto.StoreReadDto;
+import com.example.gtable.store.dto.StoreReadResponse;
+import com.example.gtable.store.dto.StoreUpdateRequest;
 
 public interface StoreService {
 
-    StoreCreateResponse createStore(StoreCreateRequest request);
+	StoreCreateResponse createStore(StoreCreateRequest request);
+
+	StoreReadResponse getAllStores();
+
+	StoreReadDto getStoreByStoreId(Long storeId);
+
+	StoreReadDto updateStore(Long storeId, StoreUpdateRequest request);
+
+	String deleteStore(Long storeId);
+
 }
