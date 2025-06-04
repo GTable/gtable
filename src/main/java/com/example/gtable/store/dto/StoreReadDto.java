efa11eaa59e1d8aa642d43314fe3a524eacd871f
Jihun Kim <zh1229@naver.com>
2025-06-04 20:15:41 +0900

    refactor(store): remove imageUrl, add List<StoreImageUploadResponse>

--- src/main/java/com/example/gtable/store/dto/StoreReadDto.java
+++ src/main/java/com/example/gtable/store/dto/StoreReadDto.java
@@ -1,8 +1,10 @@
 package com.example.gtable.store.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.example.gtable.store.model.Store;
+import com.example.gtable.storeImage.dto.StoreImageUploadResponse;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,12 +19,12 @@ public class StoreReadDto {
 	private String name;
 	private String location;
 	private String description;
-	private String storeImageUrl;
+	private List<StoreImageUploadResponse> images;
 	private Boolean isActive;
 	private Boolean deleted;
 	private LocalDateTime createdAt;
 
-	public static StoreReadDto fromEntity(Store store) {
+	public static StoreReadDto fromEntity(Store store, List<StoreImageUploadResponse> images) {
 		return StoreReadDto.builder()
 			.createdAt(store.getCreatedAt())
 			.storeId(store.getStoreId())
@@ -30,9 +32,9 @@ public class StoreReadDto {
 			.name(store.getName())
 			.location(store.getLocation())
 			.description(store.getDescription())
-			.storeImageUrl(store.getStoreImageUrl())
 			.isActive(store.getIsActive())
 			.deleted(store.getDeleted())
+			.images(images)
 			.build();
 	}
 }
