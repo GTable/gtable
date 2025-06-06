ddd9263ccfac4f6e9c7e66e677cf32f2a5ab1cc4
Jihun Kim <zh1229@naver.com>
2025-06-06 17:44:06 +0900

    feat(menu): add MenuCreateResponse dto with fromEntity method

---
+++ src/main/java/com/example/gtable/menu/dto/MenuCreateResponse.java
@@ -0,0 +1,32 @@
+package com.example.gtable.menu.dto;
+
+import java.time.LocalDateTime;
+
+import com.example.gtable.menu.model.Menu;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class MenuCreateResponse {
+	private Long menuId;
+	private Long storeId;
+	private String name;
+	private String description;
+	private String price;
+	private LocalDateTime createdAt;
+
+	public static MenuCreateResponse fromEntity(Menu menu) {
+		return MenuCreateResponse.builder()
+			.createdAt(menu.getCreatedAt())
+			.menuId(menu.getId())
+			.storeId(menu.getStoreId())
+			.name(menu.getName())
+			.description(menu.getDescription())
+			.price(menu.getPrice())
+			.build();
+	}
+}
