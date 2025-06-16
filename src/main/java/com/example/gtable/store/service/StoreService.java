c2bd197a819d7c23fc6b6de12746ea92fee0f16c
Jihun Kim <zh1229@naver.com>
2025-06-17 00:52:09 +0900

    refactor: 변경 코드 반영

--- src/main/java/com/example/gtable/store/service/StoreService.java
+++ src/main/java/com/example/gtable/store/service/StoreService.java
@@ -1,13 +1,10 @@
 package com.example.gtable.store.service;
 
-import java.util.List;
-
 import com.example.gtable.store.dto.StoreCreateRequest;
 import com.example.gtable.store.dto.StoreCreateResponse;
 import com.example.gtable.store.dto.StoreReadDto;
 import com.example.gtable.store.dto.StoreReadResponse;
 import com.example.gtable.store.dto.StoreUpdateRequest;
-import com.example.gtable.store.model.Store;
 
 public interface StoreService {
 
@@ -20,7 +17,5 @@ public interface StoreService {
 	StoreReadDto updateStore(Long storeId, StoreUpdateRequest request);
 
 	String deleteStore(Long storeId);
-
-	List<StoreReadDto> searchStoresByName(String name);
 
 }
